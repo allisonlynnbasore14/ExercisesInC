66c9566ff51cf03a5508f43dfd9ef25b09705de4
Allison <allisonlynnbasore14@gmail.com>
2019-01-26 20:16:13 -0500

    added hello.c optimization analysis

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    // int y = x + 1;
+    // printf("%i\n",y);
     return 0;
 }
+
+/*
+  Optimization using -O2:
+    In these experiments, the -O2 optimization was run with the main() function
+    along with running the main() function without any optimization. Using the
+    -O2 optimization appeared to less of the 'movl' command than without
+    optimization.
+
+    Next, the main() function was run with the two lines shown uncommented.
+    This causes the unoptimized function's assembly code to become more verbose
+    with 'movl' commands. The increase to the O2 optimization did not increase
+    significantly.
+
+    I believe the O2 optimization combined the x, 1, and y without having to
+    save them in seperate registers. Therefore, with the optimization it will
+    take less space and time to calcuate and print y.
+
+*/
