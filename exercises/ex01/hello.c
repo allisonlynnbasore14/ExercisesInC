ae38a42ce6fd3c85ecf54f82f2098c1919cc2bda
Allison <allisonlynnbasore14@gmail.com>
2019-02-01 13:24:42 -0500

    added exersizes to hello file

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
 
+
+int newFunc(){
+  return 3 + 4;
+}
+
 int main() {
     int x = 5;
     // int y = x + 1;
     // printf("%i\n",y);
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    printf("c is %d\n", c);
+
+    if (c%2 == 0) {
+        printf("c is even\n");
+    } else {
+        printf("c is odd\n");
+    }
+
+    int val = newFunc();
+
     return 0;
 }
+
+
 
 /*
   Optimization using -O2:
     In these experiments, the -O2 optimization was run with the main() function
-    along with running the main() function without any optimization. Using the
+    along with running th e main() function without any optimization. Using the
     -O2 optimization appeared to less of the 'movl' command than without
     optimization.
-
     Next, the main() function was run with the two lines shown uncommented.
     This causes the unoptimized function's assembly code to become more verbose
     with 'movl' commands. The increase to the O2 optimization did not increase
     significantly.
-
     I believe the O2 optimization combined the x, 1, and y without having to
     save them in seperate registers. Therefore, with the optimization it will
     take less space and time to calcuate and print y.
-
 */
