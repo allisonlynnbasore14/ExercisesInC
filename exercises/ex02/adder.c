29b59794d6b14f312a1a4f7a933118f76f59ab64
Allison Basore <allisonbasore@Allisons-MacBook-Air.local>
2019-02-02 13:47:59 -0500

    added final comments to working adder file

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -1,41 +1,92 @@
+
+/*
+  Adding User Input Values
+
+  The user inputs number values
+  After the user is done inputing values, the sum of values is calculated
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define SIZE 11
 
-int sumArray(int *array , int size){
+
+/*Sums the interger contents of the given array
+
+  For each value in the given array refernce, the value of sum is totaled
+
+  *array: a refernce to the array to be summed
+*/
+
+int sumArray(int *array){
   int sum = 0;
   int i;
-  for(i=0;i<size;i++){
+  for(i=0;i<SIZE;i++){
     sum = sum + array[i];
   }
   return sum;
 }
 
+/*Prints an error of a given type
+
+  For the errortype, prints a specific message for the user
+
+  errorType: the type of error (1,2, or 3)
+*/
+
+void printError(int errorType){
+
+  switch(errorType){
+    case 1:
+      printf("Sorry, that input is too large. Try something smaller.\n");
+      break;
+    case 2:
+      printf("Sorry that input is invalid, please enter a non-zero integer.\n");
+      break;
+    case 3:
+      printf("That is all the numbers we have space for!\n");
+      break;
+    default:
+      printf("Sorry that dosen't work. Try again or press Control + D\n");
+  }
+}
+
+/*Sums the values of user inputed numbers
+
+  Stores user inputed values in an array
+  prints errors if wrong type, too many inputs, or too large of an input
+  Sums the array
+
+*/
+
 
 int main(){
-  int i;
-  int size = 5;
-  int array[size];
-  for (i=0; i<size; i++) {
-      array[i] = 3;
-  }
-
-
-  char *text = calloc(1,1), buffer[10];
-  printf("Enter a message: \n");
-  while( fgets(buffer, 10 , stdin) ) /* break with ^D or ^Z */
+  int arrIndex = 0;
+  int arr[SIZE];
+  char inBuffer[10];
+  printf("Please enter your numbers to be added: \n");
+  while(fgets(inBuffer, sizeof(inBuffer), stdin))
   {
-    text = realloc( text, strlen(text)+1+strlen(buffer) );
-    //if( !text ) ... /* error handling */
-    strcat( text, buffer ); /* note a '\n' is appended here everytime */
-    printf("%s\n", buffer);
-  }
-  printf("\ntext:\n%s",text);
+    int val = atoi(inBuffer);
+    if(strchr(inBuffer, '\n') == NULL){
+      int c;
+      while((c = getc(stdin)) != '\n' && c != EOF);
+      printError(1);
+    }else if(val == 0){
+      printError(2);
+    }else{
+      arr[arrIndex] = val;
+      arrIndex++;
+    }
+    if(arrIndex == (sizeof(arr)/sizeof(int))-1){
+      printError(3);
+      break;
+    }
 
-  // char buf[10];
-  // fgets(buf, 10, stdin);
-  // printf("string is: %s\n", buf);
-  int sum = sumArray(array, size);
-  printf("%d\n", sum);
+  }
+  int sum = sumArray(arr);
+  printf("Here is your sum: %d\n", sum);
   return 0;
 }
