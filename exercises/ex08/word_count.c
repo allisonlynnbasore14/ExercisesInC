04ed1569d2503a73b03220f87dc1d35abd96b144
Allison <allisonlynnbasore14@gmail.com>
2019-04-06 02:01:04 -0400

    added comments

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -8,56 +8,37 @@
 
 
 GHashTable* hash;
-// gpointer p = (void*) 1;
-// gpointer p2 = (void*) 1;
-// g_hash_table_insert(hash,"Jazzy",p);
-// g_hash_table_insert(hash,"Mr Darcy",p2);
 
+/* read_words: reads the words seperated by whitespaces in a passed in text file reference
+
+  reads text file from argument reference
+  checks if word is new and inserts word in hashtable or increments word count
+
+*/
 
 void read_words (FILE *f) {
     char x[1024];
     while (fscanf(f, " %1023s", x) == 1) {
-        // puts(x);
-        // char *old_key;
-        // gpointer old_value;
         gpointer old_key;
         gpointer old_value;
 
         if (g_hash_table_lookup_extended (hash, strdup(x), &old_key, &old_value)){
-            /* Insert the new value */
             gpointer p = old_value;
             int val = GPOINTER_TO_INT(p) + 1;
             gpointer pNew = (void*) val;
             g_hash_table_insert (hash, strdup(x), pNew);
-            /* Just free the key and value */
         }else{
-            /* Insert into our hash table it is not a duplicate. */
             gpointer p = (void*) 1;
             g_hash_table_insert (hash,strdup(x), p);
         }
     }
 }
 
+/* printUniqueWords: function passed to foreach function to print each key pair value
 
-void readText(){
-  // FILE *file;
-  // file=fopen("test.txt", "r");
-  //
-  // if(NULL == file){
-  //   printf("\n Error opening file");
-  //   return;
-  // }
-  // int c;
-  // while(1) {
-  //    c = fgetc(file);
-  //    if( feof(file) ) {
-  //       break ;
-  //    }
-  //    // lineSplit(c);
-  //
-  // }
-  // fclose(file);
-}
+  casts the key and value and prints pair
+
+*/
 
 void printUniqueWords(gpointer key, gpointer value, gpointer user_data){
   int realVal = GPOINTER_TO_INT( value );
@@ -71,24 +52,15 @@ void printUniqueWords(gpointer key, gpointer value, gpointer user_data){
 
 int main(int argc, char** argv) {
 
-    // printf("There are %d keys in the hash table\n", g_hash_table_size(hash));
-    //
-    // printf("Jazzy likes %d\n", (int)g_hash_table_lookup(hash, "Jazzy"));
-
-
-    // readText();
     hash = g_hash_table_new(g_str_hash, g_str_equal);
     FILE *file;
     file=fopen("CountFile.txt", "r");
     read_words(file);
     fclose(file);
 
-    // void iterator(gpointer key, gpointer value, gpointer user_data) {
     g_hash_table_foreach( hash, printUniqueWords, NULL );
-    // }
-
 
     g_hash_table_destroy(hash);
-    // countFreq()
+
     return 0;
 }
