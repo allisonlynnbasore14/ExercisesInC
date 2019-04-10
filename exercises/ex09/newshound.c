11e061a4ae8ae92f7031ad768508918dcdb45355
Allison Basore <allisonbasore@Allisons-MacBook-Air.local>
2019-04-09 23:37:44 -0400

    added working newshoud.c

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -11,7 +11,7 @@ Modified by Allen Downey.
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <wait.h>
+#include <sys/wait.h>
 
 
 void error(char *msg)
@@ -22,11 +22,14 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
+    int status;
+    pid_t pid;
+    int i;
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <search phrase>\n", argv[0]);
         return 1;
     }
-    const char *PYTHON = "/usr/bin/python2";
+    const char *PYTHON = "/usr/bin/python2.6";
     const char *SCRIPT = "rssgossip.py";
     char *feeds[] = {
         "http://www.nytimes.com/services/xml/rss/nyt/Africa.xml",
@@ -39,14 +42,38 @@ int main(int argc, char *argv[])
     char *search_phrase = argv[1];
     char var[255];
 
-    for (int i=0; i<num_feeds; i++) {
+    for (i=0; i<num_feeds; i++) {
+        printf("%s\n", "AT A NEW FEED");
         sprintf(var, "RSS_FEED=%s", feeds[i]);
         char *vars[] = {var, NULL};
+        pid = fork();
 
-        int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
-        if (res == -1) {
-            error("Can't run script.");
+        if (pid == -1) {
+            fprintf(stderr, "fork failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+        if(!pid){
+          int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
+          if (res == -1) {
+              error("Can't run script.");
+          }
         }
     }
+    for (i=0; i<num_feeds; i++) {
+        pid = wait(&status);
+
+        if (pid == -1) {
+            fprintf(stderr, "wait failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+
+        // check the exit status of the child
+        status = WEXITSTATUS(status);
+
+    }
+
+    exit(0);
     return 0;
 }
